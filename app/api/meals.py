fca17860fe6885ac4067a241cacd8bf38d74880a
Andrew Hynes <andrewjhynes@gmail.com>
2023-06-25 12:03:21 -0230

    feat: get meals by ingredient

--- app/api/meals.py
+++ app/api/meals.py
@@ -1,16 +1,31 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from sqlmodel import Session
 from sqlmodel.sql.expression import select
 
 from app.api.util import get_session
 from app.models.meal import Meal
+from app.models.ingredient import Ingredient
+from typing import Annotated
+from app.models.meal_ingredient_link import MealIngredientLink
 
 router = APIRouter()
 
 
 @router.get("/meals")
-def get_meals(session: Session = Depends(get_session)):
+def get_meals(
+    session: Session = Depends(get_session),
+    ingredients: Annotated[list[str] | None, Query()] = None,
+):
     statement = select(Meal)
+
+    if ingredients is not None:
+        statement = statement.join(
+            MealIngredientLink, Meal.id == MealIngredientLink.meal_id
+        ).join(Ingredient, MealIngredientLink.ingredient_id == Ingredient.id)
+
+        for ingredient in ingredients:
+            statement = statement.where(Ingredient.name == ingredient)
+
     meals = session.exec(statement).all()
 
     return meals
--- test/api/test_meals.py
+++ test/api/test_meals.py
@@ -2,6 +2,7 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlmodel import Session
 from app.models.meal import Meal
+from app.models.ingredient import Ingredient
 
 
 @pytest.mark.parametrize("name", ["Peanut satay ramen", "Kkanpoong tofu"])
@@ -31,4 +32,24 @@ def test_get_meals(session: Session, client: TestClient):
         {"id": 1, "name": names[0]},
         {"id": 2, "name": names[1]},
         {"id": 3, "name": names[2]},
+    ]
+
+
+def test_get_meals_by_ingredient(session: Session, client: TestClient):
+    ingredient = Ingredient(name="Chickpeas")
+
+    meals = [Meal(name=name) for name in ["Hummus", "Falafel", "PB&J"]]
+
+    meals[0].ingredients.append(ingredient)
+    meals[1].ingredients.append(ingredient)
+
+    session.add_all(meals)
+    session.commit()
+
+    response = client.get("/meals", params={"ingredients": ["Chickpeas"]})
+
+    assert response.status_code == 200
+    assert response.json() == [
+        {"id": 1, "name": "Hummus"},
+        {"id": 2, "name": "Falafel"},
     ]
