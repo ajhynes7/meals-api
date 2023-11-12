1a5df9696ab1493d692a016a0707f9bf51783bf3
Andrew Hynes <andrew.hynes@colabsoftware.com>
2023-11-12 17:05:25 -0330

    feat: get ingredients by nutrients

--- app/api/ingredients.py
+++ app/api/ingredients.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter, Depends
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Query
 from sqlmodel import Session
 from sqlmodel.sql.expression import select
 
 from app.api.util import get_session
-from app.models.ingredient import Ingredient
+from app.models.ingredient import Ingredient, IngredientRead
+from app.models.ingredient_nutrient_link import IngredientNutrientLink
+from app.models.nutrient import Nutrient
 
 router = APIRouter()
 
@@ -16,8 +20,27 @@ def get_ingredient(
 
 
 @router.get("/ingredients")
-def get_ingredients(session: Session = Depends(get_session)) -> list[Ingredient]:
-    statement = select(Ingredient)
+def get_ingredients(
+    session: Session = Depends(get_session),
+    nutrients: Annotated[list[str] | None, Query()] = None,
+) -> list[IngredientRead]:
+    statement = select(Ingredient).order_by(Ingredient.name)
+
+    if nutrients is not None:
+        statement = statement.join(
+            IngredientNutrientLink,
+            Ingredient.id == IngredientNutrientLink.ingredient_id,
+        ).join(Nutrient, IngredientNutrientLink.nutrient_id == Nutrient.id)
+
+        statements_to_intersect = []
+
+        for nutrient in nutrients:
+            statements_to_intersect.append(statement.where(Nutrient.name == nutrient))
+
+        statement = statements_to_intersect[0].intersect_all(
+            *statements_to_intersect[1:]
+        )
+
     ingredients = session.exec(statement).all()
 
     return ingredients
--- test/api/test_get_ingredients.py
+++ test/api/test_get_ingredients.py
@@ -3,6 +3,7 @@ from fastapi.testclient import TestClient
 from sqlmodel import Session
 
 from app.models.ingredient import Ingredient
+from app.models.nutrient import Nutrient
 
 
 @pytest.mark.parametrize("name", ["Red lentils", "Black beans"])
@@ -30,4 +31,47 @@ def test_get_ingredients(session: Session, client: TestClient):
         {"id": 1, "name": names[0]},
         {"id": 2, "name": names[1]},
         {"id": 3, "name": names[2]},
+    ]
+
+
+def test_get_ingredients_by_nutrient(session: Session, client: TestClient):
+    nutrient = Nutrient(name="Vitamin C")
+
+    ingredients = [
+        Ingredient(name=name) for name in ["Orange", "Grapefruit", "Cashews"]
+    ]
+
+    ingredients[0].nutrients.append(nutrient)
+    ingredients[1].nutrients.append(nutrient)
+
+    session.add_all(ingredients)
+    session.commit()
+
+    response = client.get("/ingredients", params={"nutrients": ["Vitamin C"]})
+
+    assert response.status_code == 200
+    assert response.json() == [
+        {"id": 2, "name": "Grapefruit"},
+        {"id": 1, "name": "Orange"},
+    ]
+
+
+def test_get_ingredients_by_multiple_nutrients(session: Session, client: TestClient):
+    nutrients = [Nutrient(name=name) for name in ["Vitamin A", "Vitamin C"]]
+    ingredients = [Ingredient(name=name) for name in ["Grapefruit", "Clementine"]]
+
+    ingredients[0].nutrients.append(nutrients[0])
+    ingredients[0].nutrients.append(nutrients[1])
+    ingredients[1].nutrients.append(nutrients[1])
+
+    session.add_all(ingredients)
+    session.commit()
+
+    response = client.get(
+        "/ingredients", params={"nutrients": ["Vitamin A", "Vitamin C"]}
+    )
+
+    assert response.status_code == 200
+    assert response.json() == [
+        {"id": 1, "name": "Grapefruit"},
     ]
